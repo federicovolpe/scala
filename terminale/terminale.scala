f9345e2ccd0714f71b4458e245248a186bc60553
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-07-12 14:17:12 +0200

    gitignore and terminale working

--- terminale/terminale.scala
+++ terminale/terminale.scala
@@ -1,45 +1,58 @@
-import scala.util.parsing.combinator._                                                                                                          
-                                                                                                                                                
-sealed trait Comando                                                                                                                            
-//i path in questo caso sono composti da un singolo nome quindi string, altrimenti devono essere cambiati in List[String]                       
-case class Ls   (p: List[String]) extends Comando                                                                                                        
-case class Cd   (p: List[String]) extends Comando                                                                                                        
-case class Touch(p: List[String]) extends Comando                                                                                                        
-case class Mkdir(p: List[String]) extends Comando                                                                                                        
-                                                                                                                                                
-object TParser extends JavaTokenParsers{                                                                                                        
-    def parse: Parser[List[Comando]] = rep1(comando) ^^ {                                                                                      
-        case c => List(c)                                                                                                                 
-        case _ => throw new java.lang.RuntimeException("qualcosa in parse è andato male")                                                       
-    }                                                                                                                                 
-                                                                                                                                      
-    //parser per un comando                                                                                                           
-    def comando : Parser[Comando] = ("touch" | "ls" | "cd" | "mkdir") ~ path ^^{                                                            
-        case c ~ p => c match{                                                                                                                  
-                            case "touch" => Touch(p)                                                                                                           
-                            case "ls"    => Ls(p)                                                                                           
-                            case "cd"    => Cd(p)                                                                                           
-                            case "mkdir" => Mkdir(p)                                                                                                           
-                            }                                                                                                               
-        case _ => throw new java.lang.IllegalArgumentException("errore nel parser comando")                                                 
-    }                                                                                                                                       
-                                                                                                                                            
-    //riconoscitore per il path                                                                                                             
-    def path : Parser[List[String]] = rep1("/" ~> stringLiteral) ^^{                                                                        
-        case p => p                                                                                                                         
-        case _ => throw new IllegalArgumentException("il parser del path non ha funzionato a dovere")                                       
-    }                                                                                                                                       
-                                                                                                                                            
-}                                                                                                                                           
-                                                                                                                                            
-object terminale {                                                                                                                          
-    def main (args: Array[String]): Unit ={                                                                                                 
-        //args.foreach(file =>                                                                                                              
-            //val testo = io.Source.fromFile(file)).mkString                                                                                
-            val input = io.StdIn.readLine()                                                                                                 
-            TParser.parseAll(TParser.path, input) match{                                                                                    
-                case TParser.Success(risultato, _) => println("risultato parsato con successo: " + risultato)                              
-                                                                                                                                           
-                case e => println("FALLIMENTO") }                                                                                          
-    }                                                                                                                                      
-}                                                                                                                                          
+import scala.util.parsing.combinator._                                                                                                                                
+                                                                                                                                                                      
+sealed trait Comando                                                                                                                                                  
+//i path in questo caso sono composti da un singolo nome quindi string, altrimenti devono essere cambiati in List[String]                                             
+case class Ls            () extends Comando                                                                                                                           
+case class Cd   (p: String) extends Comando                                                                                                                           
+case class Touch(p: String) extends Comando                                                                                                                           
+case class Mkdir(p: String) extends Comando                                                                                                                           
+                                                                                                                                                                      
+object TParser extends JavaTokenParsers{                                                                                                                              
+    def parse: Parser[List[Comando]] = rep1(comando) 
+                                                                                                                                                                                      
+    //parser per un comando                                                                                                                                                           
+    def comando : Parser[Comando] = {                             
+        ("touch" | "cd" | "mkdir") ~ ident ^^{                                                                                                                                                                             
+            case c ~ p => c match{                                                                                                                                                        
+                                case "touch" =>                                                                                                                                           
+                                    println("riconosciuto il comando touch " + p)                                                                                                         
+                                    Touch(p)                                                                                                                                         
+                                case "cd"    =>                                                                                                                                           
+                                    println("riconosciuto il comando cd " + p)                                                                                                            
+                                    Cd(p)                                                                                                                                                 
+                                case "mkdir" =>                                                                                                                                           
+                                    println("riconosciuto il comando mkdir " + p)                                                                                                                
+                                    Mkdir(p)                                                                                                                                                     
+                                }                                                                                                                                                                 
+            case _ => throw new java.lang.IllegalArgumentException("errore nel parser comando")                                                                                                   
+        }    |                                                       
+        "ls" ^^ {case c => println("riconosciuto il comando ls ")                                                                                                            
+                                    Ls()                            
+                   }                                                
+    }                                                                                                                                                                          
+                                                                                                                                                                                              
+    //riconoscitore per il path                                                                                                                                                               
+    /*def path : Parser[List[String]] = (rep1("/" ~> stringLiteral) | stringLiteral) ^^ {                                                                                                     
+    def path : Parser[String] = (rep1("/" ~> stringLiteral) | stringLiteral) ^^ {                                                                                                             
+        case path if path.isInstanceOf[List[String]] =>                                                                                                                                       
+            println("riconosciuto il path: " + path.asInstanceOf[List[String]]) ;path                                                                                                         
+        case path if path.isInstanceOf[String] =>                                                                                                                                             
+            println("riconosciuto il path: " + path.asInstanceOf[String]) ;path                                                                                                               
+        case _ => throw new java.lang.IllegalArgumentException("qualcosa è adato storto nel path")                                                                                                                                                                                
+    } */                                                                                                                                                                                      
+                                                                                                                                                                                              
+}                                                                                                                                                                                             
+                                                                                                                                                                                              
+object terminale {                                                                                                                                                                            
+    def main (args: Array[String]): Unit ={                                                                                                                                                   
+        args.foreach{file =>                                                                                                                                                                  
+            var testo = io.Source.fromFile(file).mkString                
+            println("testo trovato : " + testo)                                                                                                                                               
+            TParser.parseAll(TParser.parse, testo) match{                                                                                                                                     
+                case TParser.Success(risultato, _) => println("risultato parsato con successo: " + risultato)                                                                                 
+                                                                                                                                                                                              
+                case TParser.NoSuccess(msg, _) => println("FALLIMENTO: "+ msg)                                                                                                                
+            }                                                                                                                                                                                 
+        }                                                                                                                                                                                    
+    }                                                                                                                                                                                        
+}                                                                                                                                                                                            
